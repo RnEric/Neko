ee590fd205ecaa6075c88f498f3f2257c01e7d05
Eric Ren <ericrren@outlook.com>
2022-02-09 23:02:17 +0800

    大幅精简代码

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,32 +14,12 @@ namespace Neko
         public MainWindow()
         {
             InitializeComponent();
-            ori_feeling.Value = random_generator(0, Convert.ToInt32(ori_feeling.Maximum) + 1);
+            ori_feeling.Value = random_generator(1, Convert.ToInt32(ori_feeling.Maximum) + 1);
             ori_feeling_value.Text = ori_feeling.Value.ToString();
-            rambutan_feeling.Value = random_generator(0, Convert.ToInt32(rambutan_feeling.Maximum) + 1);
+            ori_feeling_value_100.Text = feeling_handle(0, 0, 0, "ori", ori_feeling.Value)[0];
+            rambutan_feeling.Value = random_generator(1, Convert.ToInt32(rambutan_feeling.Maximum) + 1);
             rambutan_feeling_value.Text = rambutan_feeling.Value.ToString();
-            if (ori_feeling.Value == 100)
-            {
-                ori_feeling_value_100.Text = value_100("ori");
-            }
-            else
-            {
-                ori_feeling_value_100.Text = "";
-            }
-            if (rambutan_feeling.Value == 100)
-            {
-                rambutan_feeling_value_100.Text = value_100("毛丹");
-            }
-            else
-            {
-                rambutan_feeling_value_100.Text = "";
-            }
-        }
-
-        public static string value_100(string member_name)
-        {
-            string[] nya_meow = { "nya~", "喵~" };
-            return member_name + "最喜欢主人了！" + nya_meow[random_generator(0, 2)];
+            rambutan_feeling_value_100.Text = feeling_handle(0, 0, 0, "毛丹", rambutan_feeling.Value)[0];
         }
 
         public static void random_message(string which_json)
@@ -56,19 +36,52 @@ namespace Neko
             }
         }
 
+        public static string[] feeling_handle(int min, int max, double what_number, string member_name, double preset_number)
+        {
+            string[] nya_meow = { "nya~", "喵~" };
+            string reaction_100 = member_name + "最喜欢主人了！" + nya_meow[random_generator(0, 2)];
+            if (preset_number == -1)
+            {
+                int feeling_change = random_generator(min, max + 1);
+                string plus = (feeling_change + what_number).ToString();
+                string minus = (what_number + feeling_change).ToString();
+                string[] value_calculation = { plus, minus };
+                double feeling_handle_final = Convert.ToDouble(value_calculation[random_generator(0, 2)]);
+                if (feeling_handle_final > 100)
+                {
+                    string[] content_100 = { 100.ToString(), reaction_100 };
+                    return content_100;
+                }
+                else if (feeling_handle_final < 0)
+                {
+                    string[] normal_content = { 0.ToString(), "" };
+                    return normal_content;
+                }
+                else
+                {
+                    string[] normal_content = { feeling_handle_final.ToString(), "" };
+                    return normal_content;
+                }
+            }
+            else
+            {
+                if (preset_number == 100)
+                {
+                    string[] init_content = { reaction_100 };
+                    return init_content;
+                }
+                else
+                {
+                    string[] init_content = { "" };
+                    return init_content;
+                }
+            }
+        }
+
         public static int random_generator(int min, int max)
         {
             var new_random_generator = new Random();
             return new_random_generator.Next(min, max);
-        }
-
-        public static double random_feeling(int min, int max, double what_number)
-        {
-            int feeling_change = random_generator(min, max);
-            string plus = (feeling_change + what_number).ToString();
-            string minus = (what_number + feeling_change).ToString();
-            string[] value_calculation = { plus, minus };
-            return Convert.ToDouble(value_calculation[random_generator(0, 2)]);
         }
 
         private void ver_info_Click(object sender, RoutedEventArgs e)
@@ -85,38 +98,16 @@ namespace Neko
                 JArray rambutan_random_message = JArray.Parse(rambutan_json_read["random_message"].ToString());
                 JArray ori_message_ver = JArray.Parse(ori_json_read["basicinfo"].ToString());
                 JArray ori_random_message = JArray.Parse(ori_json_read["random_message"].ToString());
-                MessageBox.Show("毛丹词库版本：" + rambutan_message_ver[0] + "\n词库收录条数：" + rambutan_random_message.Count + "\n\nori词库版本：" + ori_message_ver[0] + "\n词库收录条数：" + ori_random_message.Count + "\n\n程序版本：" + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                MessageBox.Show("毛丹词库版本：" + rambutan_message_ver[0] + "\n词库收录条数：" + rambutan_random_message.Count + "\n\nori词库版本：" + ori_message_ver[0] + "\n词库收录条数：" + ori_random_message.Count + "\n\n程序版本：" + Assembly.GetExecutingAssembly().GetName().Version.ToString(), "", 0, MessageBoxImage.Asterisk);
             }
         }
 
         private void rambutan_Click(object sender, RoutedEventArgs e)
         {
-            double rambutan_feeling_final = random_feeling(-20, 20, rambutan_feeling.Value);
-            if (rambutan_feeling_final > 100)
-            {
-                rambutan_feeling.Value = 100;
-                rambutan_feeling_value.Text = rambutan_feeling.Value.ToString();
-                rambutan_feeling_value_100.Text = value_100("毛丹");
-            }
-            else if (rambutan_feeling_final < 0)
-            {
-                rambutan_feeling.Value = 0;
-                rambutan_feeling_value.Text = rambutan_feeling.Value.ToString();
-                rambutan_feeling_value_100.Text = "";
-            }
-            else
-            {
-                rambutan_feeling.Value = rambutan_feeling_final;
-                rambutan_feeling_value.Text = rambutan_feeling.Value.ToString();
-                if (rambutan_feeling.Value == 100)
-                {
-                    rambutan_feeling_value_100.Text = value_100("毛丹");
-                }
-                else
-                {
-                    rambutan_feeling_value_100.Text = "";
-                }
-            }
+            string[] rambutan_feeling_final = feeling_handle(-20, 20, rambutan_feeling.Value, "毛丹", -1);
+            rambutan_feeling.Value = Convert.ToDouble(rambutan_feeling_final[0]);
+            rambutan_feeling_value.Text = rambutan_feeling.Value.ToString();
+            rambutan_feeling_value_100.Text = rambutan_feeling_final[1];
             random_message("rambutan.json");
         }
 
@@ -134,32 +125,10 @@ namespace Neko
 
         private void ori_Click(object sender, RoutedEventArgs e)
         {
-            double ori_feeling_final = random_feeling(-20, 20, ori_feeling.Value);
-            if (ori_feeling_final > 100)
-            {
-                ori_feeling.Value = 100;
-                ori_feeling_value.Text = ori_feeling.Value.ToString();
-                ori_feeling_value_100.Text = value_100("ori");
-            }
-            else if (ori_feeling_final < 0)
-            {
-                ori_feeling.Value = 0;
-                ori_feeling_value.Text = ori_feeling.Value.ToString();
-                ori_feeling_value_100.Text = "";
-            }
-            else
-            {
-                ori_feeling.Value = ori_feeling_final;
-                ori_feeling_value.Text = ori_feeling.Value.ToString();
-                if (ori_feeling.Value == 100)
-                {
-                    ori_feeling_value_100.Text = value_100("ori");
-                }
-                else
-                {
-                    ori_feeling_value_100.Text = "";
-                }
-            }
+            string[] ori_feeling_final = feeling_handle(-20, 20, ori_feeling.Value, "ori", -1);
+            ori_feeling.Value = Convert.ToDouble(ori_feeling_final[0]);
+            ori_feeling_value.Text = ori_feeling.Value.ToString();
+            ori_feeling_value_100.Text = ori_feeling_final[1];
             random_message("ori.json");
         }
     }
